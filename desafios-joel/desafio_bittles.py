83b1ac5e2b0ee97ffd99c705e3810f96da13fdfb
josemiguel33-amr25 <josemiguel.lebrero.nunez.alu@iesfernandoaguilar.es>
2025-10-29 10:22:41 +0100

    Delete desafios-joel directory

--- desafios-joel/desafio_bittles.py
+++
@@ -1,9 +0,0 @@
-def calcular_numero_bolos(numero):
-    if numero == 1:
-        return 1
-    return numero + calcular_numero_bolos(numero-1)
-if __name__ == "__main__":  
-    casos_prueba = int(input())
-    for _ in range(casos_prueba):
-        num_filas = int(input())
-        print(calcular_numero_bolos(num_filas))
--- desafios-joel/desafio_boxes_f1.py
+++
@@ -1,8 +0,0 @@
-num_casos_prueba = int(input())
-
-for _ in range(num_casos_prueba):
-    numero_pilotos = int(input())
-    pilotos = input().split()
-    posiciones = map(int,input().split())
-
-    
